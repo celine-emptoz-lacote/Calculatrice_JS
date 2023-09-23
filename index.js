d1e5a8b9f74b3c2d978a5acf2aeeac2ea9a7236a
celine-emptoz-lacote <celine.emptoz-lacote@laplateforme.io>
2023-09-23 18:02:33 +0200

    Creation d une pop up pour l affichage de l historique

--- index.js
+++ index.js
@@ -8,41 +8,64 @@ document.addEventListener('DOMContentLoaded', () => {
     for ( let i = 0; i< btns.length; i++){
 
         btns[i].addEventListener('click', () => {  
-            
-            if (reset == true){
-                operation = ""
-                resultat[0].innerHTML = operation
-                reset = false
-            }
 
-            switch(btns[i].getAttribute('id')) {
-                case "moins": 
-                    operation += "-"
-                    break
-                case "plus":  
-                    operation += "+"
-                    break
-                case "diviser": 
-                    operation += "/"
-                    break
-                case "multiplier": 
-                    operation += "*"
-                    break
-                case "egal":
-                    operation = eval(operation)
-                    reset = true
-                    break
-                case "supp":
-                    operation = "0"
-                    reset = true
-                    break
-                default:
-                    operation += btns[i].getAttribute('id')
-
-            }
+            if (btns[i].getAttribute('id') !== 'fermer' && btns[i].getAttribute('id') !== "pop"  ){
+                if (reset == true){
+                    operation = ""
+                    resultat[0].innerHTML = operation
+                    reset = false
+                }
     
-            resultat[0].innerHTML = operation
+                switch(btns[i].getAttribute('id')) {
+                    case "moins": 
+                        operation += "-"
+                        break
+                    case "plus":  
+                        operation += "+"
+                        break
+                    case "diviser": 
+                        operation += "/"
+                        break
+                    case "multiplier": 
+                        operation += "*"
+                        break
+                    case "egal":
+                        operation = eval(operation)
+                        reset = true
+                        break
+                    case "supp":
+                        operation = "0"
+                        reset = true
+                        break
+                    default:
+                        operation += btns[i].getAttribute('id')
+    
+                }
+        
+                resultat[0].innerHTML = operation
+            }else{
+                console.log('fermer')
+            }
+
+            // Pop Up
+            let popup = document.getElementsByClassName('popup')
+
+            // Ouverture de la pop up
+            if (btns[i].getAttribute('id') == "pop"){
+                popup[0].style.display = 'block'
+            }
+
+            // Fermeture de la pop up
+            if (btns[i].getAttribute('id') == "fermer"){
+                popup[0].style.display = 'none'
+            }
+
+
+
+           
         })
+
+        
     }
     
     
