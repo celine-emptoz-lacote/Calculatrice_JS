022203a90bc0a120ba7388bc10f21a85943529e3
celine-emptoz-lacote <celine.emptoz-lacote@laplateforme.io>
2023-09-24 16:48:14 +0200

    Ajout de la fonctionnalité convertisseur

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let btns = document.querySelectorAll('button')
     var resultat = document.getElementsByClassName('view')
     let historique = document.getElementById('historique')
+    let selection = document.getElementById('selection')
     let operation = ""
     let reset = false
+    let open = false
+
 
     for ( let i = 0; i< btns.length; i++){
 
         btns[i].addEventListener('click', () => {  
 
-            if (btns[i].getAttribute('id') !== 'fermer' && btns[i].getAttribute('id') !== "pop"  && btns[i].getAttribute('id') !== "effacer" ){
+            if (btns[i].getAttribute('id') !== 'fermer' && btns[i].getAttribute('id') !== "pop"  && btns[i].getAttribute('id') !== "effacer" && btns[i].getAttribute('id') !== "convertion"){
                 if (reset == true){
                     operation = ""
                     resultat[0].innerHTML = operation
@@ -70,14 +73,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 historique.innerHTML = ""
             }
 
+            if (btns[i].getAttribute('id') == "convertion"){
+               if (open == false){
 
+                    btns[i].innerHTML = "Fermer"
+                    resultat[1].style.display = "flex"
+                    open = true
+                    selection.style.display  = "block"
 
-           
+                    // On cache les btn 
+                    btns[4].style.display = "none"
+                    btns[8].style.display = "none"
+                    btns[12].style.display = "none"
+                    btns[16].style.display = "none"
+                }
+                else {
+                    btns[i].innerHTML = "Convertion"
+                    open = false
+                    resultat[1].style.display = "none"
+                    selection.style.display  = "none"
+                    btns[4].style.display = "inline"
+                    btns[8].style.display = "inline"
+                    btns[12].style.display = "inline"
+                    btns[16].style.display = "inline"
+                } 
+            }
+            // VAleur par defaut du select donc dollar
+            resultat[1].innerHTML = eval(operation*1.07)
         })
 
         
     }
-    
-    
 
+    selection.addEventListener("change", (e) => {
+        if (e.target.value == "dollar"){
+            resultat[1].innerHTML = eval(operation*1.07)
+        }else{
+            resultat[1].innerHTML = eval(operation*1000)
+        }
+    })
 })
